be8f545a3be2dd3ceeabd99d09246511b4785da5
joshua7736 <87724818+joshua7736@users.noreply.github.com>
2021-07-28 12:19:18 -0500

    Update PoolNetworkCode.py

--- PoolNetworkCode.py
+++ PoolNetworkCode.py
@@ -11,26 +11,26 @@ class Pool(object):
         if NEID in self._NE_list:
             print("Network element {} already in pool {}.".format(NEID.name, self.name))
             return -1
-        else:
-            #Add the NE to the Pool
-            self._NE_list.append(NEID)
-            
-            #Associate the PoolID with NE also for easy access
-            NEID.poolID = self
-            
-            return 0
+        
+        #Add the NE to the Pool
+        self._NE_list.append(NEID)
+
+        #Associate the PoolID with NE also for easy access
+        NEID.poolID = self
+
+        return 0
         
     '''
         This API is used to del a network-element level from the pool
     '''
     def DelNetworkElementInPool(self, NEID):
         if NEID in self._NE_list:
-            NED.poolID = None
+            NEID.poolID = None
             del self._NE_list[self._NE_list.index(NEID)]
             return 0
-        else:
-            print("Network Element {} not in pool {}".format(NEID.name, self.name))
-            return -1
+        
+        print("Network Element {} not in pool {}".format(NEID.name, self.name))
+        return -1
 
     '''
         This API is used to display the poolID + all the NE's in the Pool
@@ -48,51 +48,60 @@ class NetworkElement(object):
         self.name = name
         self._password = "admin"
         self.poolID = None
-        self.APN_list = []
+        self._APN_list = []
+        self.down_APNs = []
 
     def Login(self):
         if input("Password: ") == self._password:
             return True
+        
+        print("Incorrect password. Access denied.")
         return False
 
     def changePassword(self, old_password, new_password):
         #Didn't get this part
         if old_password == self._password:
             self._password = new_password
+            
         else:
-            print("Access denied")
+            print("Incorrect password.")
             
     def ExecuteCLI(self, CLI):
         self.Login()
         
-    def  ShowDetails(self):
+    def ShowDetails(self):
         #shows details of NE, poolID, neighbor NE in same pool
         print("NE-Name  :" + self.name)
-        poolID = self.poolID
-        print("Pool ID: " + poolID.name)
-        print('Neighbour NEs : ', end='')
-        for NE in poolID._NE_list:
-            if NE != self:
-                print(NE.name + ',', end= '')
-        #for new line 
-        print('')   
+        
+        if self.poolID == None:
+            print("Not associated with a pool.")
+            
+        else:
+            poolID = self.poolID
+            print("Pool ID: " + poolID.name)
+            print('Neighbour NEs : ', end='')
+            for NE in poolID._NE_list:
+                if NE != self:
+                    print(NE.name + ',', end= '')
+            #for new line 
+            print('')   
             
     def AssociateAPN(self, APN):
         if APN in self._APN_list:
             print("APN {} already associated with NE {}".format(APN.name, NE.name))
             return -1
-        else:
-            #Associate the APN to NE
-            self._APN_list.append(APN)
-            return 0
+
+        #Associate the APN to NE
+        self._APN_list.append(APN)
+        return 0
          
     def DissociateAPN(self, APN):
         if APN in self._APN_list:
             self._APN_list.remove(APN)
             return 0
-        else:
-            print("APN {} not associated with NE {}".format(APN.name, NE.name))
-            return -1
+
+        print("APN {} not associated with NE {}".format(APN.name, NE.name))
+        return -1
 
 class AccessPoint(object):
     def __init__(self, name):
@@ -108,9 +117,14 @@ class MonitorElement(object):
         self.name = name
 
     def SetAlarm(NEID, APN):
-        pass
+        for NE in NEID.poolID._NE_list:
+            if NE != NEID and NE.Login():
+                NE.down_APNs.append([NEID, APN])
+    
     def ClearAlarm(NEID, APN):
-        pass
+        for NE in NEID.poolID._NE_list:
+            if NE != NEID and NE.Login():
+                NE.down_APNs.pop([NEID, APN])
 
 def main():
     NE1=NetworkElement("NE1")
